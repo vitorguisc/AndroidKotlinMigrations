7be8c0d38a9e7223a36eb298d1a71505feb6aa68
Vitor <vitorguilherme@geb.inatel.br>
2022-06-29 12:03:56 -0300

    commit de envio

---
+++ README.md
@@ -0,0 +1,6 @@
+Utilizando Migration e conhecendo sua importância de permitir o crescimento do projeto
+Salvando dados primitivos com preferences, utilizando a biblioteca DataStore
+Implementando um fluxo de autenticação de usuário, utilizando preferences
+Reutilizei a lógica entre as Activities, usando uma Activity Base
+Configurei um relacionamento entre entidades com o Room
+Fiz coma que o app salve e busque produtos de um usuário com relacionamento entre entidades
---
+++ app/src/main/java/br/com/alura/orgs/database/AppDatabase.kt
@@ -0,0 +1,44 @@
+package br.com.alura.orgs.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import br.com.alura.orgs.database.converter.Converters
+import br.com.alura.orgs.database.dao.ProdutoDao
+import br.com.alura.orgs.database.dao.UsuarioDao
+import br.com.alura.orgs.model.Produto
+import br.com.alura.orgs.model.Usuario
+
+@Database(
+    entities = [
+        Produto::class,
+        Usuario::class
+    ],
+    version = 2,
+    exportSchema = true
+)
+@TypeConverters(Converters::class)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun produtoDao(): ProdutoDao
+
+    abstract fun usuarioDao(): UsuarioDao
+
+    companion object {
+
+        @Volatile
+        private var db: AppDatabase? = null
+        fun instancia(context: Context): AppDatabase {
+            return db ?: Room.databaseBuilder(
+                context,
+                AppDatabase::class.java,
+                "orgs.db"
+            ).addMigrations(MIGRATION_1_2)
+                .build().also {
+                db = it
+            }
+        }
+    }
+}
